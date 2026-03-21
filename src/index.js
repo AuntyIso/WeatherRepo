350ed898f99cf0a656be591d0a5e626f42bbccd1
gracecare <graceisocare@gmail.com>
2026-03-21 22:06:01 +0000

    Api Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+ function searchCity(city) {
+    let apikey = "7a4f5a11f027034d307t909d5doccba8";
+    let apiurl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric';
+    axios.get(apiurl).then(refreshWeather);
+
+} 
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input"); 
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 
 }
 
     let searchFormElement = document.querySelector("#search-form"); 
     searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-});
