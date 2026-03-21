cbbdafee071e5ca5713f720f4f371f074589e49f
gracecare <graceisocare@gmail.com>
2026-03-21 15:57:45 +0000

    corrections to search engine

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ function handleSearchSubmit(event) {
     let searchInput = document.querySelector("#search-form-input"); 
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
-    
+
 }
 
-  
-let searchFormElement = document.querySelector("#search-form"); 
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+    let searchFormElement = document.querySelector("#search-form"); 
+    searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+});
