fa954bda4646fd3adf435039ea4deafba6023369
gracecare <graceisocare@gmail.com>
2026-03-23 18:08:36 +0000

    Added API search functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+ function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature-value");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(temperature);
+    let cityElement = document.innerHTML = querySelector("#city");
+
+ }
+ 
+ 
  function searchCity(city) {
     let apikey = "7a4f5a11f027034d307t909d5doccba8";
     let apiurl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric';
@@ -14,7 +23,9 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 
 }
-
+  
     let searchFormElement = document.querySelector("#search-form"); 
     searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+    searchCity("Paris");
 
